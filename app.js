dd253a6257eaf6b581adeb47300e0e8ba7a22896
ls <cancel>
2021-07-30 16:21:22 -0500

    modaldirection

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 console.log("working")
+
+//coding a pop up.
+//grab the game button
+const openBtn = document.querySelector(".modal-buttons");
+
+//grabbing the modal
+const modal = document.querySelector(".modal");
+const modal1 = document.querySelector(".modal1");
+
+//grabbig the close button
+const close = document.getElementById("close");
+
+
+//Function to change modal display to block
+const openModal = () => {
+    modal.style.display = "block";
+}
+console.log("modal working?")
+
+const closeModal = () => {
+    modal.style.display = "none"
+}
+
+//adding event listener
+openBtn.addEventListener("click", openModal)
+
+//adding event listener
+close.addEventListener("click", closeModal)
+
+//setTimeout(openModal, 2000)  - this will pop out the modal without click
+//setTimeout(closeModal, 10000) - this will remove the display modal.
