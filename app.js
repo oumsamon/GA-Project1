9576edb7468ca0cb04d56321bf2f71ab2a5f313b
ls <cancel>
2021-08-04 11:01:02 -0500

    automodalclosing

--- app.js
+++ app.js
@@ -12,6 +12,25 @@ const close3 = document.querySelector("#close3");
 const openBtn4 = document.querySelector("#openModal4");
 const modal4 = document.querySelector("#modal4");
 const close4 = document.querySelector("#close4");
+const openBtn5 = document.querySelector("#openModal5");
+const modal5 = document.querySelector("#modal5");
+const close5 = document.getElementById("close5");
+const openBtn6 = document.querySelector("#openModal6");
+const modal6 = document.querySelector("#modal6");
+const close6 = document.querySelector("#close6");
+const openBtn7 = document.querySelector("#openModal7");
+const modal7 = document.querySelector("#modal7");
+const close7 = document.querySelector("#close7");
+const openBtn8 = document.querySelector("#openModal8");
+const modal8 = document.querySelector("#modal8");
+const close8 = document.querySelector("#close8");
+const openBtn9 = document.querySelector("#openModal9");
+const modal9 = document.querySelector("#modal9");
+const close9 = document.querySelector("#close9");
+const openBtn10 = document.querySelector("#openModal10");
+const modal10 = document.querySelector("#modal10");
+const close10 = document.querySelector("#close10");
+
 const p1score = document.getElementById("p1")
 
 let player1score = 0
@@ -30,6 +49,7 @@ openBtn1.addEventListener("click", openModal1 = () => {
         console.log(turn)
     console.log(choice1)
     turn = 1
+    openBtn1.disabled = true
     } else {
         choice2 = openBtn1.value
         compareCards()
@@ -37,14 +57,14 @@ openBtn1.addEventListener("click", openModal1 = () => {
         console.log(choice2)
         turn = 0
     }
-    
+    setTimeout(closeModal, 3000)
 })
 
 close1.addEventListener("click", closeModal = () => {
     modal1.style.display = "none"
+    
 })
-
-
+ 
 openBtn2.addEventListener("click", openModal2 = () => {
     modal2.style.display = "block";
     openBtn2.classList.add("selected")
@@ -62,12 +82,12 @@ openBtn2.addEventListener("click", openModal2 = () => {
         console.log(choice2)
         turn = 0
     }
+    setTimeout(closeModal2, 3000) 
 })
 
 close2.addEventListener("click", closeModal2 = () => {
     modal2.style.display = "none";
 })
-
 
 openBtn3.addEventListener("click", openModal3 = () => {
     modal3.style.display = "block";
@@ -85,36 +105,150 @@ openBtn3.addEventListener("click", openModal3 = () => {
         console.log(choice2)
         turn = 0
     }
+    setTimeout(closeModal3, 3000)
 })
 
 close3.addEventListener("click", closeModal3 = () => {
     modal3.style.display = "none";
 })
-
-
+ 
 openBtn4.addEventListener("click", openModal4 = () => {
     modal4.style.display = "block";
     openBtn4.classList.add("selected")
     if(turn == 0) {
         choice1 = openBtn4.value
-        console.log(turn)
-      
-    console.log(choice1)
     turn = 1
     } else {
         choice2 = openBtn4.value
-        console.log(turn)
-compareCards()
-
-
-        console.log(choice2)
+        compareCards()
         turn = 0
     }
+    setTimeout(closeModal4, 3000)
 })
 
 close4.addEventListener("click", closeModal4 = () => {
     modal4.style.display = "none";
 })
+
+openBtn5.addEventListener("click", openModal5 = () => {
+    modal5.style.display = "block";
+    openBtn5.classList.add("selected")
+    if(turn == 0) {
+        choice1 = openBtn5.value
+    turn = 1
+    } else {
+        choice2 = openBtn5.value
+        compareCards()
+        turn = 0
+    }
+    setTimeout(closeModal, 3000)
+})
+
+close5.addEventListener("click", closeModal5 = () => {
+    modal5.style.display = "none";
+})
+
+openBtn6.addEventListener("click", openModal6 = () => {
+    modal6.style.display = "block";
+    openBtn6.classList.add("selected")
+    if(turn == 0) {
+        choice1 = openBtn6.value
+    turn = 1
+    } else {
+        choice2 = openBtn6.value
+        compareCards()
+        turn = 0
+    }
+    setTimeout(closeModal6, 000)
+})
+
+close6.addEventListener("click", closeModal6 = () => {
+    modal6.style.display = "none";
+})
+ 
+openBtn7.addEventListener("click", openModal7 = () => {
+    modal7.style.display = "block";
+    openBtn7.classList.add("selected")
+    if(turn == 0) {
+        choice1 = openBtn7.value
+    turn = 1
+    } else {
+        choice2 = openBtn7.value
+        compareCards()
+        turn = 0
+    }
+    setTimeout(closeModal7, 3000)
+})
+
+close7.addEventListener("click", closeModal7 = () => {
+    modal7.style.display = "none";
+})
+ 
+openBtn8.addEventListener("click", openModal8 = () => {
+    modal8.style.display = "block";
+    openBtn8.classList.add("selected")
+    if(turn == 0) {
+        choice1 = openBtn8.value
+    turn = 1
+    } else {
+        choice2 = openBtn8.value
+        compareCards()
+        turn = 0
+    }
+    setTimeout(closeModal8, 3000)
+})
+
+close8.addEventListener("click", closeModal8 = () => {
+    modal8.style.display = "none";
+})
+
+openBtn9.addEventListener("click", openModal9 = () => {
+    modal9.style.display = "block";
+    openBtn9.classList.add("selected")
+    if(turn == 0) {
+        choice1 = openBtn9.value
+    turn = 1
+    } else {
+        choice2 = openBtn9.value
+        compareCards()
+        turn = 0
+    }
+    setTimeout(closeModal9, 000)
+})
+
+close9.addEventListener("click", closeModal9 = () => {
+    modal9.style.display = "none";
+})
+ 
+openBtn10.addEventListener("click", openModal10 = () => {
+    modal10.style.display = "block";
+    openBtn10.classList.add("selected")
+    if(turn == 0) {
+        choice1 = openBtn10.value
+    turn = 1
+    } else {
+        choice2 = openBtn10.value
+        compareCards()
+        turn = 0
+    }
+    setTimeout(closeModal10, 3000)
+})
+
+close10.addEventListener("click", closeModal10 = () => {
+    modal10.style.display = "none";
+})
+ 
+
+
+
+
+
+
+
+
+
+
+
 
 
 function compareCards() {
@@ -150,21 +284,3 @@ openBtn2.classList.remove("selected");
 openBtn3.classList.remove("selected");
 openBtn4.classList.remove("selected");
 }
-
-
-
-
-
-
-
-
-// //setTimeout(openModal, 2000)  - this will pop out the modal without click
-// setTimeout(closeModal, 10000) 
-// //this will remove the display modal.
-// setTimeout(closeModal2, 10000) 
-// setTimeout(closeModal3, 10000) 
-// setTimeout(closeModal4, 10000) 
-
-// const gameReset = document.querySelector(".resetButton")
-
-// gameReset.addEventListener("click", location.reload())
