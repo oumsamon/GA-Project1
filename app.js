f5feb9d0a4cd5c6a042cd3dcfe461ff21094816e
ls <cancel>
2021-08-03 20:53:02 -0500

    comparecard

--- app.js
+++ app.js
@@ -13,28 +13,27 @@ const openBtn4 = document.querySelector("#openModal4");
 const modal4 = document.querySelector("#modal4");
 const close4 = document.querySelector("#close4");
 const p1score = document.getElementById("p1")
-const p2score = document.getElementById("p2")
-
-
 
 let player1score = 0
-let player2score = 0
 let choice1
 let choice2
 let turn = 0
 
-p2score.innerHTML = player2score
 p1score.innerHTML = player1score
 
 openBtn1.addEventListener("click", openModal1 = () => {
     modal1.style.display = "block";
+    openBtn1.classList.add("selected")
     if(turn == 0) {
-        choice1 = openBtn1.id
+        //i added value to correctly match each other for the function.
+        choice1 = openBtn1.value
+      
         console.log(turn)
     console.log(choice1)
     turn = 1
     } else {
-        choice2 = openBtn1.id
+        choice2 = openBtn1.value
+        compareCards()
         console.log(turn)
         console.log(choice2)
         turn = 0
@@ -49,13 +48,17 @@ close1.addEventListener("click", closeModal = () => {
 
 openBtn2.addEventListener("click", openModal2 = () => {
     modal2.style.display = "block";
+    openBtn2.classList.add("selected")
     if(turn == 0) {
-        choice1 = openBtn2.id
+        choice1 = openBtn2.value
+        
+       
         console.log(turn)
     console.log(choice1)
     turn = 1
     } else {
-        choice2 = openBtn2.id
+        choice2 = openBtn2.value
+        compareCards()
         console.log(turn)
         console.log(choice2)
         turn = 0
@@ -69,13 +72,16 @@ close2.addEventListener("click", closeModal2 = () => {
 
 openBtn3.addEventListener("click", openModal3 = () => {
     modal3.style.display = "block";
+    openBtn3.classList.add("selected")
     if(turn == 0) {
-        choice1 = openBtn3.id
+        choice1 = openBtn3.value
+       
         console.log(turn)
     console.log(choice1)
     turn = 1
     } else {
-        choice2 = openBtn3.id
+        choice2 = openBtn3.value
+        compareCards()
         console.log(turn)
         console.log(choice2)
         turn = 0
@@ -89,14 +95,19 @@ close3.addEventListener("click", closeModal3 = () => {
 
 openBtn4.addEventListener("click", openModal4 = () => {
     modal4.style.display = "block";
+    openBtn4.classList.add("selected")
     if(turn == 0) {
-        choice1 = openBtn4.id
+        choice1 = openBtn4.value
         console.log(turn)
+      
     console.log(choice1)
     turn = 1
     } else {
-        choice2 = openBtn4.id
+        choice2 = openBtn4.value
         console.log(turn)
+compareCards()
+
+
         console.log(choice2)
         turn = 0
     }
@@ -108,18 +119,38 @@ close4.addEventListener("click", closeModal4 = () => {
 
 
 function compareCards() {
+    console.log("compare card working?")
     if (choice1 == choice2) {
-        //console.log before and after in the terminal.
-        player1score.innerHTML(parseInt(player1score.innerHTML) + 10)
-        console.log(player1score)
+        //update player score to increment by 10
+        player1score = player1score+ 10
+        console.log("player1score inside", player1score)
+        //display the updated player score on the DOM.
+        p1score.innerHTML = player1score
+        //im selecting all of the button that have selected from DOM
+        console.log("look here for selected buttons", document.querySelectorAll(".selected"))
+        //i a grabbing the button that have selected class inside them
+        const selectedButtons = document.querySelectorAll('.selected');
+        selectedButtons.forEach(btn => {
+            //i created a var btn to represent Each of the btn that selected. then make it red.
+            btn.style.backgroundColor = "green"
+            btn.disabled = true
+
+            console.log("what is this", btn)
+        });
+    } else {
+        player1score = player1score - 10
+        console.log("player1score inside", player1score)
+        p1score.innerHTML = player1score  
     }
 
-else {
-    player1score.innerHTML(parseInt(player1score.innerHTML) - 10)
-    console.log(player1score)
+//to remove the selected class    
+openBtn1.classList.remove("selected");
+openBtn2.classList.remove("selected");
+openBtn3.classList.remove("selected");
+openBtn4.classList.remove("selected");
 }
-}
-//compareCards()
+
+
 
 
 
